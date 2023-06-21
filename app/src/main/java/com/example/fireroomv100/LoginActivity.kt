9551ee26d3d47c693d89bea690649e8d5472f929
Dan <dan.ms.0739@gmail.com>
2023-06-21 10:57:49 -0600

    Added databinding snippet to LoginActivity.kt

--- app/src/main/java/com/example/fireroomv100/LoginActivity.kt
+++ app/src/main/java/com/example/fireroomv100/LoginActivity.kt
@@ -2,10 +2,15 @@ package com.example.fireroomv100
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fireroomv100.databinding.LoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val loginBinding=LoginBinding.inflate(layoutInflater)
+        setContentView(loginBinding.root)
+        loginBinding.phoneLoginButton.setOnClickListener {
+            //handle phone login
+        }
     }
 }
